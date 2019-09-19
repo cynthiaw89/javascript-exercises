2ec456a1d80c6aac4ce1abc1a62c11f6e0b52c36
Cynthia Ware <cynthiaw1989@gmail.com>
2019-09-19 13:47:48 -0400

    Complete reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+//split string into an array
+let splitStr = str.split("");
+//reverse the array
+let reverseArray = splitStr.reverse();
+//join the array into a new string
+let joinArray = reverseArray.join("");
+//return the string
+return joinArray;
 }
-
 module.exports = reverseString
