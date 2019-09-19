9300674e98d67986ec00bf123bd05ea9ae5cbf27
Cynthia Ware <cynthiaw1989@gmail.com>
2019-09-19 13:37:32 -0400

    Complete repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
 const repeatString = function() {
-
+    let str = ""
+    for(i=-1; i<5; i++){
+        str += "hey";
+        if(i<0){
+            return "ERROR";
+        }
+    }
+    return str;
 }
 
 module.exports = repeatString
