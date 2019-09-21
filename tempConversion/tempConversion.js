266a06c3fb8530d6a99906b6c3b93d30f324af80
Cynthia Ware <cynthiaw1989@gmail.com>
2019-09-20 22:37:27 -0400

    Complete tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+x = (x - 32) * (5/9);
+x= Math.round(x * 10) / 10;
+return x;
 }
 
-const ctof = function() {
-
+const ctof = function(x) {
+x = (x * (9/5)) + 32;
+x = Math.round(x * 10) / 10;
+return x;
 }
 
 module.exports = {
